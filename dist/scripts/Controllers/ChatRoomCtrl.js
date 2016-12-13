9eede8262245de44a3b6eda1c4732138bda764e4
leogoesger <leoq91@gmail.com>
2016-12-12 22:02:05 -0800

    checkpoint-4-styling complete

--- dist/scripts/Controllers/ChatRoomCtrl.js
+++ dist/scripts/Controllers/ChatRoomCtrl.js
@@ -3,6 +3,19 @@
      	this.roomName = "Chat Test";
      	this.room = Room;
      	this.message = Message;
+     	this.class = "odd";
+
+     	this.appliedClass = function(){
+     		if(this.class){
+     			this.class = false;
+     			console.log("dd")
+     			return "odd";
+     		} else {
+     			this.class = true;
+     			console.log("ff")
+     			return "even";
+     		}
+     	}
      }
  
      angular
